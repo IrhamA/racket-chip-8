86e2a814af422292ee743e745e477e9ad99c8aec
Basile Stathopulos <bstathopulos@gmail.com>
2020-12-13 01:35:49 -0500

    add limited register support

--- emul.rkt
+++ emul.rkt
@@ -3,8 +3,13 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname emul) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;;------------------------------------------------------------------------------
 
-;; Some code here like a string
-(define program (string-append
+;; A Byte is a Nat in the range [0, 255]
+;; A Word is a Nat in the range [0, 65535]
+
+;;------------------------------------------------------------------------------
+
+;; A Program is a Str
+(define pong (string-append
   "6a026b0c6c3f6d0ca2eadab6dcd66e0022d4660368026060f015f0073000"
   "121ac717770869ffa2f0d671a2eadab6dcd66001e0a17bfe6004e0a17b02"
   "601f8b02dab6600ce0a17dfe600de0a17d02601f8d02dcd6a2f0d6718684"
@@ -15,9 +20,46 @@
   "126ca2f2fe33f265f12964146500d4557415f229d45500ee808080808080"
   "800000000000"))
 
-;; A Byte is a Nat in the range [0, 255]
+;;------------------------------------------------------------------------------
 
-;; A Progam is a Str
+;; A Registers is a
+;; (make-registers Word Word Byte Byte Byte
+;;                 Byte Byte Byte Byte Byte Byte Byte Byte
+;;                 Byte Byte Byte Byte Byte Byte Byte Byte)
+(define-struct registers (pc i  sp st dt
+                          v0 v1 v2 v3 v4 v5 v6 v7
+                          v8 v9 va vb vc vd ve vf))
+
+;; (registers-vn registers ref) returns the n-th register
+;; registers-vn: Registers Nat -> Byte
+;; Requires:
+;;     0 <= n <= 15
+(define (registers-vn registers n)
+  (case n [(00) (registers-v0 registers)]
+          [(01) (registers-v1 registers)]
+          [(02) (registers-v2 registers)]
+          [(03) (registers-v3 registers)]
+          [(04) (registers-v4 registers)]
+          [(05) (registers-v5 registers)]
+          [(06) (registers-v6 registers)]
+          [(07) (registers-v7 registers)]
+          [(08) (registers-v8 registers)]
+          [(09) (registers-v9 registers)]
+          [(10) (registers-va registers)]
+          [(11) (registers-vb registers)]
+          [(12) (registers-vc registers)]
+          [(13) (registers-vd registers)]
+          [(14) (registers-ve registers)]
+          [(15) (registers-vf registers)]))
+;; To-do: is there a nice define-syntax way to do this?
+
+;; (register-set! registers n val) sets the value of the n-th register
+;; register-set!: Registers Nat Byte -> Void
+;; Requires:
+;;     0 <= n <= 15
+;; To-do: requires #lang racket so we can use #:mutable
+
+;;------------------------------------------------------------------------------
 
 ;; A Ram is a (vectorof Byte)
 (define ram (make-vector 4096 0))
